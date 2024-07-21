9ae92714a597cf470c22459f14a66ddcdcddadb8
aryan-0603 <114023565+aryan-0603@users.noreply.github.com>
2024-07-21 23:09:28 +0530

    Create ChocolateDistribution.java

---
+++ Day5/ChocolateDistribution.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int i = 0;i<n;i++)
+                {
+                    int x = sc.nextInt();
+                    arr.add(x);
+                }
+            int m = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+    		System.out.println(ob.findMinDiff(arr,n,m));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+      Collections.sort(a);
+      long maxValue=Long.MAX_VALUE;
+      long x=m-1;
+      for(int i=0;x<n;i++)
+      {
+          long val=a.get((int)x) - a.get((int)i);
+          if(val<maxValue)
+          maxValue=val;
+          x++;
+      }
+      return maxValue;
+    }
+}
